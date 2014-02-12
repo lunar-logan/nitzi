ada4ae0753505511b92451789f2be2e4d0e9d503
lunar-logan <developerx@live.com>
2014-02-12 22:44:07 +0530

    Adding spi

---
+++ src/nitzi/httpengine/spi/Event.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nitzi.httpengine.spi;
+
+/**
+ *
+ * @author ANURAG
+ */
+public interface Event {
+    
+}
