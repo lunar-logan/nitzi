51802648fd4c5ec7c5a8fc8370e0a2ec9d5ca121
lunar-logan <developerx@live.com>
2014-02-13 19:37:17 +0530

    Added loads of kuda and kadkat ;)

--- src/nitzi/httpengine/RequestProcessor.java
+++ src/nitzi/httpengine/RequestProcessor.java
@@ -17,6 +17,8 @@
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
+ * 
+ * The main Request Processing engine
  */
 package nitzi.httpengine;
 
---
+++ src/nitzi/httpengine/spi/AbstractRequestProcessor.java
@@ -0,0 +1,29 @@
+//
+// ::text
+// Copyright 2014 Anurag Gautam
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package nitzi.httpengine.spi;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Abstract request processor engine
+ * @author ANURAG
+ */
+public interface AbstractRequestProcessor {
+    
+    public void parse(byte[] buffer) throws UnsupportedEncodingException;
+    
+}
